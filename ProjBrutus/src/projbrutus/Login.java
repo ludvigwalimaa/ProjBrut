bad9bcd77e998fc3ec2cba8114e707c9af61d175
Gabbelago <snelpiller@gmail.com>
2014-04-28 14:09:39 +0200

    Main startar login. Loginhanterare har metod för logincheck

--- ProjBrutus/src/projbrutus/Login.java
+++ ProjBrutus/src/projbrutus/Login.java
@@ -1,5 +1,28 @@
 package projbrutus;
 
+import java.util.Scanner;
+
 public class Login {
+	
+	Scanner in = new Scanner(System.in);
+	LoginManager login = new LoginManager();
+	String userID;
+	String pass;
+	
+	public Login() {
+		
+		System.out.println("Skriv in användarnamn: ");
+		userID = in.nextLine();
+		
+		System.out.println("Skriv in lösen: ");
+		pass = in.nextLine();
+		
+		if (login.loginCheck(userID, pass)) {
+			System.out.println("---Login success---");
+		} else {
+			System.out.println("---Login failed---");
+		}
+		
+	}
 
 }
--- ProjBrutus/src/projbrutus/LoginManager.java
+++ ProjBrutus/src/projbrutus/LoginManager.java
@@ -1,5 +1,17 @@
 package projbrutus;
 
 public class LoginManager extends Main {
-
-}
+	
+	public boolean loginCheck(String ID, String pass) {
+			boolean secure = false;
+			
+			if (ID.equals("admin") && pass.equals("admin")) {
+				
+				secure = true;
+				
+			}
+			
+			return secure;
+		}
+		
+	}
