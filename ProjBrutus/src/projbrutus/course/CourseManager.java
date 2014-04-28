1ba50489bd8263d8df26a3df5dba2ea02933111a
Gabbelago <snelpiller@gmail.com>
2014-04-28 15:31:47 +0200

    ändrat i metod för createcourse, fortfarande massa hyss att göra. La till medtoden createexaplats med

--- ProjBrutus/src/projbrutus/course/CourseManager.java
+++ ProjBrutus/src/projbrutus/course/CourseManager.java
@@ -1,7 +1,16 @@
 package projbrutus.course;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import projbrutus.course.examination.ExaminationArea;
+import projbrutus.course.group.Group;
+import projbrutus.person.Person;
+
 
 public class CourseManager {
+	
+	Scanner in = new Scanner(System.in);
 	
 	public CourseRoom getCourseRoom(String cID) {
 		CourseRoom k = new CourseRoom();
@@ -10,9 +19,37 @@ public class CourseManager {
 		return k;
 	}
 	
-	public void createCourseRoom(String cId, String cName) {
+	public void createCourseRoom() {
+		
+		System.out.println("Mata in kursid");
+		
+		String cId = in.nextLine();
+		
+		System.out.println("Mata in kursnamn");
+		String cName = in.nextLine();
+		
+		CourseRoom course = new CourseRoom();
+		course.setcId(cId);
+		course.setcName(cName);
+		
+	    course.setExaList(createExaminationArea());
+		
+		//skapa grupplista? ArrayList<Group> groupList
+		
+		//skapa deltagarlista? ArrayList<Person> participantList
+ 
+		//skapa kurstruktur? CourseStructure cStructure
 		
 	}
 	
+	public ArrayList<ExaminationArea> createExaminationArea(){
+		
+		System.out.println("Skriv in lite latjogrejs");
+		
+		ArrayList<ExaminationArea> exaList = null;
+
+		return exaList;
+		
+	}
 	
 }
