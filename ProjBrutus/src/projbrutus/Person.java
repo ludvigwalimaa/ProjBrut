64f62f8043315766e718bd567bf2edb854276c6c
Gabbelago <snelpiller@gmail.com>
2014-04-28 11:53:37 +0200

    person igen

--- ProjBrutus/src/projbrutus/Person.java
+++ ProjBrutus/src/projbrutus/Person.java
@@ -2,4 +2,35 @@ package projbrutus;
 
 public class Person {
 
+	private String liudID;
+	private String name;
+	private String personNummer;
+
+	public Person(String liuID, String name, String personNummer) {
+
+	}
+
+	public String getLiudID() {
+		return liudID;
+	}
+
+	public void setLiudID(String liudID) {
+		this.liudID = liudID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPersonNummer() {
+		return personNummer;
+	}
+
+	public void setPersonNummer(String personNummer) {
+		this.personNummer = personNummer;
+	}
 }
