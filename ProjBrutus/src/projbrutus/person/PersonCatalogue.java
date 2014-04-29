4278e1155ac0dd67d1b7e10787cc6cf736f9ee4a
LudvigWalimaa <ludwa930@student.liu.se>
2014-04-29 14:11:29 +0200

    False

--- ProjBrutus/src/projbrutus/person/PersonCatalogue.java
+++ ProjBrutus/src/projbrutus/person/PersonCatalogue.java
@@ -24,6 +24,7 @@ public class PersonCatalogue {
 	public boolean comparePersons(String liuID, String password) {
 		for (int i = 0; i < PCatalogue.size(); i++) {
 			if (liuID.equals(PCatalogue.get(i).getLiudID())) {
+				return false;
 			}
 			if (password.equals(PCatalogue.get(i).getPassword())) {
 				return true;
