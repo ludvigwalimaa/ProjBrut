daebab9e759d600f77c77bf6ba56cde5fb702eea
LudvigWalimaa <ludwa930@student.liu.se>
2014-04-29 14:13:28 +0200

    Fixat lite i PCatalogue

--- ProjBrutus/src/projbrutus/person/PersonCatalogue.java
+++ ProjBrutus/src/projbrutus/person/PersonCatalogue.java
@@ -14,7 +14,7 @@ public class PersonCatalogue {
 
 	private void fetchPersons() {
 		// Databas koppling till persondatabas.
-		PCatalogue.add(new Person("gabol894", "Gabriel", "0123456789", "hej123"));
+		PCatalogue.add(new Person("gabol892", "Gabriel", "0123456789", "hej123"));
 		PCatalogue.add(new Person("filbr616", "Filip", "0123456789", "hej123"));
 		PCatalogue.add(new Person("ludwa930", "Ludvig", "0123456789", "hej123"));
 		PCatalogue.add(new Person("anka101", "Andreas", "0123456789", "hej123"));
@@ -24,11 +24,10 @@ public class PersonCatalogue {
 	public boolean comparePersons(String liuID, String password) {
 		for (int i = 0; i < PCatalogue.size(); i++) {
 			if (liuID.equals(PCatalogue.get(i).getLiudID())) {
-				return false;
-			}
+
 			if (password.equals(PCatalogue.get(i).getPassword())) {
 				return true;
-			}
+			}}
 		}
 		return false;
 	}
