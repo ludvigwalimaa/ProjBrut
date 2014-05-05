e4c804a934c2f8758fd92404f6222bef0e8818f6
Gabbelago <snelpiller@gmail.com>
2014-05-05 14:26:30 +0200

    ändrade i grupplista

--- ProjBrutus/src/projbrutus/course/group/GroupList.java
+++ ProjBrutus/src/projbrutus/course/group/GroupList.java
@@ -12,6 +12,10 @@ public class GroupList {
 		
 		//sql anrop till table över gruppID samt kursID för att
 		//hämta alla grupper som tillhör en kurs
+		
+		//måste göra grupplistor för varje kurs, gör ett anrop som skapar grupplistan med
+		//tomma grupper tillsvidare
+		groupList = createGroupList(cId);
 	
 		return groupList;
 	}
@@ -19,6 +23,8 @@ public class GroupList {
 	public ArrayList<Group> createGroupList(String cId) {
 		
 		//metod som skapar lista över grupper för en kurs
+		//just nu gör denna defaultgruppsize antal grupper och lägger dem i en lista
+		//just nu går det alltså inte att skapa en grupp enskilt.
 		
 		for (int i = 0; i < cs.getDefaultGroupListSize(); i++) {
 		Group g = new Group(cId, Integer.toString(i));
