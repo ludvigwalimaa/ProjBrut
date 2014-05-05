80e026cc579871fee9026aa5ecf08aae870b8b75
Gabbelago <snelpiller@gmail.com>
2014-05-05 10:29:32 +0200

    fixat deltagare i participantlist

--- ProjBrutus/src/projbrutus/course/participant/ParticipantCatalogue.java
+++ ProjBrutus/src/projbrutus/course/participant/ParticipantCatalogue.java
@@ -1,5 +1,61 @@
 package projbrutus.course.participant;
 
+import java.util.ArrayList;
+
+import projbrutus.person.Person;
+
 public class ParticipantCatalogue {
+
+
+	//ska anropa table bestående av liuid och kursid, matchar inskickad kursid mot table
+	//och returnerar arraylista med alla liuid som tillhär dettta kursid
+
+	public ArrayList<String> getParticipant(String cId){
+		ArrayList<String> pList = new ArrayList<String>();
+
+
+		if(cId.equals("725G51")) {
+			pList.add("gabol892");
+			pList.add("ludwa930");
+			pList.add("anka101");
+		} else if (cId.equals("725G54")) {
+			pList.add("gabol892");
+		} else if (cId.equals("725G56")) {
+			pList.add("gabol892");
+		} else if (cId.equals("725G44")) {
+			pList.add("gabol892");
+		} else if (cId.equals("725G41")){		
+			pList.add("filbr616");
+		} else if (cId.equals("725G64")) {
+			pList.add("filbr616");
+		} else if (cId.equals("725G26")) {
+			pList.add("filbr616");
+		} else if (cId.equals("725G74")) {
+			pList.add("filbr616");
+			pList.add("anka101");
+		} else if (cId.equals("725G34")) {
+			pList.add("ludwa930");
+		}else if (cId.equals("725G24")) {
+			pList.add("anka101");
+		} else if (cId.equals("725G66")) {
+			pList.add("anka101");
+		} else if (cId.equals("725G74")) {
+			pList.add("anka101");
+			pList.add("filbr616");
+		} else {
+			pList.add("no participats");
+		}
+
+
+
+
+		return pList;
+	}
+
+
+
+
+
+
 
 }
--- ProjBrutus/src/projbrutus/course/participant/ParticipantList.java
+++ ProjBrutus/src/projbrutus/course/participant/ParticipantList.java
@@ -2,26 +2,25 @@ package projbrutus.course.participant;
 
 import java.util.ArrayList;
 import projbrutus.person.PersonCatalogue;
+import projbrutus.course.participant.ParticipantCatalogue;
 
 import projbrutus.course.CourseRoom;
 import projbrutus.person.Person;
 
-public class ParticipantList {
-	PersonCatalogue pc = new PersonCatalogue();
 
-	public ArrayList<Person> getParticipantList(CourseRoom c) {
-		
-		//denna deltagarlista blir nu densamma som personkatalogen. I verkligheten
-		//skulle denna metod anropa table bestående av liudID samt kursID och returnera
-		//alla personID för inskickad kursID
-		ArrayList<Person> pList = new ArrayList<Person>();
-		
-		pList.addAll(pc.getPCatalogue());
-		
+public class ParticipantList {
+	ParticipantCatalogue pc = new ParticipantCatalogue();
+
+	public ArrayList<String> getParticipantList(CourseRoom c) {
+
+		ArrayList<String> pList = new ArrayList<String>();
+
+		pList.addAll(pc.getParticipant(c.getcId()));
+
 		//metod som via en sql sats hämtar alla deltargare för en kurs och retunerar denna lista
-		
+
 		return pList;
-		
+
 	}
-	
+
 }
