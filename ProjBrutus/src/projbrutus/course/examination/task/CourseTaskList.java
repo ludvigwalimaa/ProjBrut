d3f5833dfd4e750b3f668b0434e4abdd6d4fbbb3
Gabbelago <snelpiller@gmail.com>
2014-05-06 10:24:13 +0200

    skapat courseTask samt autogenererad lista

--- ProjBrutus/src/projbrutus/course/examination/task/CourseTaskList.java
+++ ProjBrutus/src/projbrutus/course/examination/task/CourseTaskList.java
@@ -1,5 +1,22 @@
 package projbrutus.course.examination.task;
 
+import java.util.ArrayList;
+import projbrutus.course.CourseStructure;
+
 public class CourseTaskList {
+	
+	CourseStructure cs = new CourseStructure();
+	ArrayList<CourseTask> courseTaskList = new ArrayList<CourseTask>();
+	
+	public ArrayList<CourseTask> createCourseTaskList(String cId, String eId) {
+		
+		//metod som skapar lista �ver grupper f�r en kurs
+		
+		for (int i = 0; i < cs.getDefaultCourseTaskListSize(); i++) {
+		CourseTask ct = new CourseTask(cId, eId);
+		courseTaskList.add(ct);
+		}
+		return courseTaskList;
+	}
 
 }
