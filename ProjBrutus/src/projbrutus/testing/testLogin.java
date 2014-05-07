2b6b62e33081e20c4e8abdc6095a06d85af2ef2d
Gabbelago <snelpiller@gmail.com>
2014-05-07 12:15:25 +0200

    testing. Ligger ny sats för överblick, ersätt i överblick

---
+++ ProjBrutus/src/projbrutus/testing/testLogin.java
@@ -0,0 +1,17 @@
+package projbrutus.testing;
+
+import java.util.ArrayList;
+
+import projbrutus.course.examination.task.CourseTask;
+
+public class testLogin {
+	
+/*
+ * 
+ * 	if ((chosenTask <= tasks.size()) && (chosenTask > -1 )) {
+			
+			System.out.println(tasks.get(chosenTask).toString());
+			manageTask(tasks.get(chosenTask));
+		}
+ */
+}
