f45cdb646fcae1b5d24a00783f8ad118aaff32c4
LudvigWalimaa <ludwa930@student.liu.se>
2014-05-14 15:10:02 +0200

    Skapat CalcGrade som ska räkna ut ett betyg

--- ProjBrutus/src/projbrutus/course/examination/task/CalcGrade.java
+++ ProjBrutus/src/projbrutus/course/examination/task/CalcGrade.java
@@ -1,5 +1,27 @@
 package projbrutus.course.examination.task;
 
-public class CalcGrade {
+import projbrutus.course.CourseRoom;
 
+public class CalcGrade {
+	
+	private CourseRoom cr;
+	//Skapa GradeStructure i CourseStructure.
+	
+	public CalcGrade(CourseRoom cr){
+		this.cr = cr;
+	}
+
+//	private void checkGradeSys(){
+//		if (){ 
+//			
+//			/* If CR har betygsskalan för FilFak */ 
+//		
+//		}else{ 
+//			
+//			/* Else if CR har betygsskalan för TekFak */ 
+//		}	
+//				
+//}
 }
+
+
